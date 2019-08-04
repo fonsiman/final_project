8791ef29b47f25e87c0a2901b825036afd237ead
Fonsiman <aromanbonachera@gmail.com>
2019-08-04 15:26:19 +0200

    probando open cv

--- main.py
+++ main.py
@@ -1,11 +1,67 @@
 import cv2
+import numpy as np
 import time
 from random import randint
+from keras.models import load_model
+
+def pinball():
+    while True:
+
+        '''    img_processed = processImage(frame)
+        
+        prediction = int(model.predict_classes((np.array(img_processed))))
+        
+        print(prediction)'''
+
+        if direccion == 0 or direccion == 3:
+            width += 5
+        else:
+            width -= 5
+        if direccion == 0 or direccion == 1:
+            height += 5
+        else:
+            height -= 5
+
+        ret, frame = cap.read()
+        cv2.circle(frame, (width, height), 10, (0, 0, 255), 16)
+
+        cv2.imshow('object_detection', frame)
+
+        # Rebotes en las paredes
+        if height == (frame.shape[0] - 10) and direccion == 0:
+            direccion = 3
+        elif height == (frame.shape[0] - 10):
+            direccion = 2
+
+        if height == (10) and direccion == 3:
+            direccion = 0
+        elif height == (10):
+            direccion = 1
+
+        if width == (frame.shape[1] - 10):
+            ret, frame = cap.read()
+            cv2.line(frame, (width*2-50, 0), (width*2-10, height*2), (0, 0, 255), 20)
+            cv2.imshow('object_detection', frame)
+            time.sleep(2)
+            direccion = randint(0, 3)
+            width = frame.shape[1]  // 2
+            height = frame.shape[0]  // 2
+
+        if width == (10):
+            ret, frame = cap.read()
+            cv2.line(frame, (0, 0), (0, frame.shape[0]), (0, 0, 255), 20)
+            cv2.imshow('object_detection', frame)
+            time.sleep(2)
+            direccion = randint(0, 3)
+            width = frame.shape[1]  // 2
+            height = frame.shape[0]  // 2
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 threshold = 0.8
 
-class_names = {0: 'Palma',
-              1: 'Thumb', 2: 'Victoria', 3: 'Índice', 4: 'Rock&Roll', 5: 'OK'}
+class_names = {0: 'Palma', 1: 'Thumb', 2: 'Victoria', 3: 'Indice', 4: 'RockNRoll', 5: 'OK'}
 
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
@@ -13,56 +69,34 @@ direccion = randint(0, 3)
 width = frame.shape[1] // 2
 height = frame.shape[0] // 2
 
+model = load_model('models/second_model.h5')
+#model = cv2.dnn.readNetFromTensorflow('models/second_model.h5')
+
+def predict_rgb_image(img):
+    return model.predict(img)
+
+def process_img(img):
+    X = []
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Converts into the corret colorspace (GRAY)
+    img = cv2.resize(img, (320, 120)) # Reduce image size so training can be faster
+    X.append(img)
+
+    X = np.array(X, dtype="uint8")
+    X = X.reshape(1, 120, 320, 1)
+
+    return X
+
 while True:
 
-    '''    img_processed = processImage(frame)
-    
-    prediction = int(model.predict_classes((np.array(img_processed))))
-    
-    print(prediction)'''
-
-    if direccion == 0 or direccion == 3:
-        width += 5
-    else:
-        width -= 5
-    if direccion == 0 or direccion == 1:
-        height += 5
-    else:
-        height -= 5
-
     ret, frame = cap.read()
-    cv2.circle(frame, (width, height), 10, (255, 255, 255), 16)
 
+    predictions = predict_rgb_image(process_img(frame))
+
+    best_pred = np.argmax(predictions, axis=1)
+    print(predictions[0][best_pred], best_pred)
+    if (predictions[0][best_pred] > .8):
+        pass
     cv2.imshow('object_detection', frame)
-
-    # Rebotes en las paredes
-    if height == (frame.shape[0] - 10) and direccion == 0:
-        direccion = 3
-    elif height == (frame.shape[0] - 10):
-        direccion = 2
-
-    if height == (10) and direccion == 3:
-        direccion = 0
-    elif height == (10):
-        direccion = 1
-
-    if width == (frame.shape[1] - 10):
-        ret, frame = cap.read()
-        cv2.line(frame, (width*2-50, 0), (width*2-10, height*2), (0, 0, 255), 20)
-        cv2.imshow('object_detection', frame)
-        time.sleep(2)
-        direccion = randint(0, 3)
-        width = frame.shape[1]  // 2
-        height = frame.shape[0]  // 2
-
-    if width == (10):
-        ret, frame = cap.read()
-        cv2.line(frame, (0, 0), (0, frame.shape[0]), (0, 0, 255), 20)
-        cv2.imshow('object_detection', frame)
-        time.sleep(2)
-        direccion = randint(0, 3)
-        width = frame.shape[1]  // 2
-        height = frame.shape[0]  // 2
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break

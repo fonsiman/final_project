d7b426e11de4ade36ae9afd6883c98ae2f4b6ca8
Fonsiman <aromanbonachera@gmail.com>
2019-08-07 18:27:24 +0200

    Metiendo funcionalidades

--- prueba.py
+++ prueba.py
@@ -1,20 +1,5 @@
-from git import Repo
+cont = {"nothing": 0}
 
-PATH_OF_GIT_REPO = r'/home/alfonso/ironhack/final_project/.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'Guardado automático por gestos'
+print(list(cont.keys())[0])
 
-def git_push():
-    try:
-
-        repo = Repo(PATH_OF_GIT_REPO)  # if repo is CWD just do '.'
-        repo.index.add(["main.py"])
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote('origin')
-        origin.push()
-
-        print('Added, commited & pushed')
-
-    except:
-        print('Some error occured while pushing the code')
-
-git_push()
+print(cont["nothing"] + 1)
--- spotify.py
+++ spotify.py
@@ -1,13 +1,17 @@
-import spotipy
+'''from spotify_local import SpotifyLocal
 
-urn = 'spotify:artist:3jOstUTkEu2JkjvRdBA5Gu'
-sp = spotipy.Spotify()
+with SpotifyLocal() as s:
+        print("Conectado!")
+'''
+from time import sleep
+bashCommand = "spotify  --uri='spotify:track:03bYLN5H3OjZ6CIpBcd4W3'"
+bashCommand2 = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play"
 
-sp.trace = True # turn on tracing
-sp.trace_out = True # turn on trace out
+import subprocess
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
 
-artist = sp.artist(urn)
-print(artist)
+sleep(3)
+process2 = subprocess.Popen(bashCommand2.split(), stdout=subprocess.PIPE)
+output, error = process2.communicate()
 
-user = sp.user('plamere')
-print(user)
